4ab018dd8a6d6c8e51987e667ed424d9dd3ef2c3
patelraghav7482 <patelraghav7482@gmail.com>
2025-08-14 15:19:19 +0530

    Add files via upload

---
+++ code.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter your number",a);
+scanf("%d",&a);
+if(a>35)
+printf("enter your number is grater than 35");
+
+}
---
+++ mark raghav.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+
+int main(){
+int a,b,c;
+
+printf("enter your marks of maths",a);
+scanf("%d",&a);
+printf("enter your marks of physicas",b);
+scanf("%d",&b);
+printf("enter your marks of chemistry",c);
+scanf("%d",&c);
+
+if(a>35){
+   printf("you been pramoted to next class");
+   }
+else if (b>35)
+{
+    printf("you been pramoted to next class");
+   }
+
+else if (c>35)
+{
+    printf("you been pramoted to next class");
+}
+
+}
+
+
+
