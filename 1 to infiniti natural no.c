099a820d5e26ad6eba3456e68f502da7f84642d0
patelraghav7482 <patelraghav7482@gmail.com>
2025-08-21 15:39:32 +0530

    21 08 2025

---
+++ 1 to infiniti natural no.c
@@ -0,0 +1,14 @@
+ #include<stdio.h>
+int main(){
+int n,i;
+printf("enter a no :");
+scanf("%d",&n);
+printf("natural numbers from 1 to %d are:\n ",n);
+for(i=1;i<=n;i++){
+printf("%d\n",i);
+
+
+}
+return 0;
+
+}
