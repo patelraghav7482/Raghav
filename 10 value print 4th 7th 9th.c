d84806e204aaf39efb9b43a112662f6ac2f03f3a
patelraghav7482 <patelraghav7482@gmail.com>
2025-09-25 15:46:18 +0530

    25/9/25

---
+++ 10 value print 4th 7th 9th.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int a[10];
+int i;
+for(i=0;i<10;i++){
+scanf("%d",&a[i]);}
+ printf("4 th value %d\n",a[3]);
+ printf("7th value  %d\n",a[7]);
+ printf("9 th value %d\n",a[8]);
+
+
+
+}
+
---
+++ 5 VALUE PRINT ARRAY.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main(){
+int a[5];
+int i;
+
+for(i=1;i<=5;i++){
+      printf("value %d=",i);
+    scanf("%d",&a[i]);
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
